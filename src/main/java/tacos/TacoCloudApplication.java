995d77691f95fb0c18dda6552246e4d9bf7121be
Grand <maks.bel98@mail.ru>
2025-04-19 02:42:26 +0700

    до 60 какой-то там стр

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -1,4 +1,4 @@
-package sia.taco_cloud;
+package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/test/java/tacos/TacoCloudApplicationTest.java
@@ -0,0 +1,12 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TacoCloudApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
